836794fe49347e80cfe005ac4113f89e50bd752f
sagnik2010 <sagnikkundu25@gmail.com>
2020-12-24 18:42:24 -0500

    twitter-api

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const Twitter = require('./api/helpers/twitter')
+const twitter = new Twitter()
+const port = 3000
+require('dotenv').config()
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+app.get('/tweets', (req, res) => {
+    const query = req.query.q
+    const count = req.query.count
+    const maxId = req.query.max_id
+    twitter.get(query, count, maxId).then((response) => {
+        res.status(200).send(response.data)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.listen(port, () => console.log(`Twitter API listening on port ${port}!`))
